304f314da9d9533c15493b1f25119aff76842626
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-10-02 11:57:11 +0600

    hand burger icon toggle done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
+import { Bars4Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
+    const navbarData = [
+        {
+            id: 1,
+            name: "Home",
+            path: '/home'
+        },
+        {
+            id: 2,
+            name: "About",
+            path: '/about'
+        },
+        {
+            id: 3,
+            name: "Services",
+            path: '/services'
+        },
+        {
+            id: 4,
+            name: "Blog",
+            path: '/blog'
+        },
+        {
+            id: 5,
+            name: "Contact",
+            path: '/contact'
+        },
+    ]
+
+
+    const [open, setOpen] = useState(false)
     return (
         <header className='text-2xl bg-blue-400 text-white h-20 flex justify-between items-center px-20'>
             <div className="logo">
@@ -9,12 +40,28 @@ const Navbar = () => {
                 font-bold'>Navbar-1</h2>
             </div>
             <nav className='hidden lg:block'>
-                <a className='ml-5 font-semibold duration-500 hover:text-gray-700' href="/home">Home</a>
-                <a className='ml-5 font-semibold duration-500 hover:text-gray-700' href="/about">About</a>
-                <a className='ml-5 font-semibold duration-500 hover:text-gray-700' href="/services">Services</a>
-                <a className='ml-5 font-semibold duration-500 hover:text-gray-700' href="/blog">Blog</a>
-                <a className='ml-5 font-semibold duration-500 hover:text-gray-700' href="/contact">Contact</a>
+                {
+                    navbarData.map((navItem) =>
+                        <a className='ml-5 font-semibold duration-500 hover:text-gray-700'
+                            key={navItem.id}
+                            href={navItem.path}
+                        >
+                            {navItem.name}</a>
+                    )
+                }
+
             </nav>
+            <div className="responsive_menu_icon lg:hidden"
+                onClick={() => setOpen(!open)}
+            >
+                {
+                    open ?
+                        <XMarkIcon className="h-7 w-7 text-white" />
+                        : <Bars4Icon className="h-7 w-7 text-white" />
+                }
+
+
+            </div>
         </header>
     );
 };
